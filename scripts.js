b6d2fc740a42f064aad90d5741ab8970131196ea
Marco Suarez <jacomasmix@gmail.com>
2016-03-20 15:45:01 -0500

    EERORES CORREGIDOS

--- scripts.js
+++ scripts.js
@@ -1,8 +1,4 @@
-document.write("<h1>Hola. JavaScript!</h1>");
+alert("HOLA JAVASCRIPTS.0");
+document.write("<h1>Hola JavaScript!</h1>");
 
-dialog("<h1>Hola. 2</h1>";
-
-alert("<h1>Hola. 3</h1>")
-
-console.log("<h1>Hola. 4</h1>);
-
+alert("HOLA JAVASCRIPTS.0");
