6ebd43b9236b5b19eba6867ff39a7491957b1e99
Marco Suarez <jacomasmix@hotmail.com>
2016-03-27 09:40:26 -0500

    cuenta cuentos

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
-alert("HOLA JAVASCRIPTS.0");
-document.write("<h1>Hola JavaScript!</h1>");
-
-alert("HOLA JAVASCRIPTS.0");
+console.log("Inicio del Programa");
+var frase1 = prompt("Hola usuario, Ingrese Sustantivo!");
+var frase2 = prompt("Hola usuario, Ingrese adjetivo!");
+var frase3 = prompt("Hola usuario, Ingrese verbo!");
+document.write("<h1>CUENTO!</h1>");
+document.write("<h2>El " + frase1 +" de razas "  + frase2 +" pueden vivir "+ frase3+"</h2> ");
+document.write("<h2>Feroses " + frase2 + " el " +  frase1 +" pueden vivir " + frase3+"</h2> ");
+document.write("<h2>pueden vivir " + frase3 +" de razas " + frase2 +" El "+ frase1+" </h2>");
